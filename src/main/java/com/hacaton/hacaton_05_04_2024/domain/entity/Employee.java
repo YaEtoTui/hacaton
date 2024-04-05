06657f57f69f44724eb193b8659313950952563f
YaEtoTui <sazhin2003.egor@gmail.com>
2024-04-05 21:07:22 +0500

    fix table "employee"

--- src/main/java/com/hacaton/hacaton_05_04_2024/domain/entity/Employee.java
+++ src/main/java/com/hacaton/hacaton_05_04_2024/domain/entity/Employee.java
@@ -1,5 +1,6 @@
 package com.hacaton.hacaton_05_04_2024.domain.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
@@ -9,10 +10,16 @@ import lombok.RequiredArgsConstructor;
 @Table(name = "employee")
 @RequiredArgsConstructor
 public class Employee {
+    @Column(name = "Имя")
+    String name;
+    @Column(name = "Фамилия")
+    String secondName;
+    @Column(name = "Команда")
+    String team;
+    @Column(name = "Должность")
+    String post;
+    @Column(name = "Почта")
     @Id
     String email;
-    String name;
-    String secondname;
-    String dolchnost;
-    String team;
+
 }
